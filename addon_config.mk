8b30b7c0dbd83756cee16be8e381fda054f508c5
Mach1 <dylan@mach1studios.com>
2020-12-28 13:05:49 -0500

    Update addon_config.mk
    update to addon desc

--- addon_config.mk
+++ addon_config.mk
@@ -1,8 +1,8 @@
 meta:
 	ADDON_NAME = ofxWitmotion
-	ADDON_DESCRIPTION = Wit Motion controller openFrameworks addon
-	ADDON_AUTHOR = Qaotech Studio
-	ADDON_TAGS = "witmotion" "hardware"
+	ADDON_DESCRIPTION = WitMotion IMU controller openFrameworks addon
+	ADDON_AUTHOR = Mach1 and Qaotech Studio
+	ADDON_TAGS = "witmotion" "hardware" "imu"
 	ADDON_URL = 
 
 common:
