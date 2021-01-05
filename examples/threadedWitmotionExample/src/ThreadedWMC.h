22066d73bc6493cc371cb96928634971f9b5b8fa
Dylan Marcus <dylan@mach1studios.com>
2021-01-05 12:14:39 -0500

    init for threaded example to improve sensor rate

---
+++ examples/threadedWitmotionExample/src/ThreadedWMC.h
@@ -0,0 +1,85 @@
+//
+//  ThreadedWMC.h
+//  threadedWitmotionExample
+//
+//  Created by Dylan Marcus on 1/5/21.
+//
+
+#ifndef ThreadedWMC_h
+#define ThreadedWMC_h
+
+#include "ofMain.h"
+#include <atomic>
+
+class ThreadedWMC: public ofThread
+{
+public:
+    witmotionController wmc;
+
+    ~ThreadedWMC(){
+        stop();
+        waitForThread(false);
+    }
+
+    void setup(){
+        wmc.setup();
+        start();
+    }
+
+    /// Start the thread.
+    void start(){
+        startThread();
+    }
+
+    void stop(){
+        std::unique_lock<std::mutex> lck(mutex);
+        stopThread();
+        condition.notify_all();
+    }
+
+    void threadedFunction(){
+        while(isThreadRunning()){
+            // since we are only writting to the frame number from one thread
+            // and there's no calculations that depend on it we can just write to
+            // it without locking
+            threadFrameNum++;
+
+            // Lock the mutex until the end of the block, until the closing }
+            // in which this variable is contained or we unlock it explicitly
+            std::unique_lock<std::mutex> lock(mutex);
+
+            // The mutex is now locked so we can modify
+            // the shared memory without problem
+
+            // Now we wait for the main thread to finish
+            // with this frame until we can generate a new one
+            // This sleeps the thread until the condition is signaled
+            // and unlocks the mutex so the main thread can lock it
+            condition.wait(lock);
+        }
+    }
+
+    void update(){
+        // if we didn't lock here we would see
+        // tearing as the thread would be updating
+        // the pixels while we upload them to the texture
+        std::unique_lock<std::mutex> lock(mutex);
+        wmc.update();
+        condition.notify_all();
+    }
+
+    void updateNoLock(){
+        condition.notify_all();
+    }
+
+    int getThreadFrameNum(){
+        return threadFrameNum;
+    }
+    
+protected:
+    std::condition_variable condition;
+    int threadFrameNum = 0;
+};
+
+
+#endif /* ThreadedWMC_h */
---
+++ examples/threadedWitmotionExample/src/ofApp.cpp
@@ -0,0 +1,122 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetFrameRate(200);
+    threadedwmc.setup();
+    threadedwmc.start();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    threadedwmc.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetBackgroundColor(30);
+    
+    if(!threadedwmc.wmc.isConnected){
+        ofSetColor(255);
+        for(int i = 0; i < threadedwmc.wmc.deviceIDs.size(); i++){
+            float posY = (i + 1) * 50;
+            ofDrawBitmapString("[" + ofToString(i) + "]: " + threadedwmc.wmc.deviceIDs[i], 100, posY);
+        }
+    }else{
+        ofPushMatrix();
+        ofNoFill();
+        ofTranslate(ofGetWidth()*.5, ofGetHeight()*.5, 0);
+        
+        // rotate
+        float* a = threadedwmc.wmc.getAngle();
+        
+        float rX = a[0];
+        float rY = a[1];
+        float rZ = a[2];
+        
+        ofRotateXDeg(rX);
+        ofRotateYDeg(rY);
+        ofRotateZDeg(rZ);
+
+        ofDrawBox(0, 0, 0, 200);
+        ofPopMatrix();
+ 
+        // information text
+        ofSetColor(255);
+        ofDrawBitmapString("Gyro", 50, 20);
+        ofDrawBitmapString("Acc", 200, 20);
+        ofDrawBitmapString("Magnetic", 400, 20);
+        ofDrawBitmapString("Angle", 600, 20);
+
+        for(int i =0; i < 3; i++){
+            ofDrawBitmapString(threadedwmc.wmc.gyro[i], 50, (i + 1) * 50);
+            ofDrawBitmapString(threadedwmc.wmc.acceleration[i], 200, (i + 1) * 50);
+            ofDrawBitmapString(threadedwmc.wmc.magnetic_field[i], 400, (i + 1) * 50);
+            ofDrawBitmapString(threadedwmc.wmc.angle[i], 600, (i + 1) * 50);
+        }
+    }
+    
+    auto threadFrame = threadedwmc.getThreadFrameNum();
+    ofSetColor(255,0,0);
+    ofDrawBitmapString("app frame: " + ofToString(ofGetFrameNum()), 20,20);
+    ofDrawBitmapString("thread frame: " + ofToString(threadFrame), 20,35);
+    ofDrawBitmapString("diff: " + ofToString(int64_t(ofGetFrameNum()) - threadFrame), 20,50);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if(!threadedwmc.wmc.isConnected && key >= 48 && key <= 57) {
+        threadedwmc.wmc.connectDevice(key - 48);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    //reset orientation
+    threadedwmc.wmc.tare();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
